2c7f672713dd95b01ff6f9bee885e5636e3d8374
Juri Berlanda <juriberlanda@hotmail.com>
2023-08-31 15:02:00 +0200

    [Go] Upgrade to controller-runtime v0.16.0.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	desecv1 "github.com/j-be/desec-dns-operator/api/v1"
 	"github.com/j-be/desec-dns-operator/controllers"
@@ -67,8 +68,7 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Metrics:                server.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "b15515b6.owly.dedyn.io",
